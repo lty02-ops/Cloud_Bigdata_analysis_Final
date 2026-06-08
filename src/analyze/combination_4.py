c8e6332ab15ae3cb50075ba79ddc42e2f5ddbb8b
tuffh <tuffh@users.noreply.github.com>
2026-06-08 17:45:34 +0900

    modifying_results

--- src/analyze/combination_4.py
+++ src/analyze/combination_4.py
@@ -3,7 +3,7 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("../../results/combination_count.csv")
+df = pd.read_csv("results/combination_count.csv")
 
 top5 = (df[df["combo_size"] >= 4].sort_values("count", ascending=False).head(5))
 
@@ -12,6 +12,5 @@ plt.bar(top5["combination"], top5["count"])
 plt.title("combination >=_4")
 plt.xticks(rotation=45)
 plt.tight_layout()
-
-plt.savefig("../../results/combo_size4plus_top5.png")
+plt.savefig("results/combo_size4plus_top5.png")
 plt.show()
--- src/analyze/combination_all.py
+++ src/analyze/combination_all.py
@@ -3,7 +3,7 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("../../results/combination_count.csv")
+df = pd.read_csv("results/combination_count.csv")
 
 top10 = df.sort_values("count", ascending=False).head(10)
 
@@ -12,6 +12,5 @@ plt.bar(top10["combination"], top10["count"])
 plt.title("All combination")
 plt.xticks(rotation=45)
 plt.tight_layout()
-
-plt.savefig("../../results/all_combination_top10.png")
+plt.savefig("results/all_combination_top10.png")
 plt.show()
